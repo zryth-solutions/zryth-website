361cad29b08bdcd824f91b736ff52c601f13c249
codewithkushagra <kushagraagra008@gmail.com>
2025-09-09 00:44:20 +0530

    updated team

--- src/components/Faq/index.tsx
+++ src/components/Faq/index.tsx
@@ -4,7 +4,7 @@ import { DotPattern } from "../../assets/svg";
 
 const Faq = () => {
   return (
-    <section id="faq" className="relative z-20 overflow-hidden bg-white pb-8 pt-20 dark:bg-dark lg:pb-[50px] lg:pt-[120px]">
+    <section id="faq" className="relative z-20 overflow-hidden bg-white pb-8 pt-12 dark:bg-dark lg:pb-[50px] lg:pt-[80px]">
       <div className="container">
         <SectionTitle
           subtitle="FAQ"
